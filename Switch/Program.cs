b38549d3aca1c7d963fea638eccf53153ed40778
Gerhard Pegel <gpegel@gmail.com>
2015-09-13 22:38:27 +0200

    My first C# console application about using switches

---
+++ Switch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in a super hero's name to see his nickname: ");
+            string userValue = Console.ReadLine();
+
+            switch (userValue.ToUpper())
+            {
+                case "BATMAN":
+                    Console.WriteLine("Caped Crusader");
+                    break;
+                case "SUPERMAN":
+                    Console.WriteLine("Man of Steel");
+                    break;
+                case "GREENLANTARN":
+                    Console.WriteLine("Emerald Knight");
+                    break;
+                default:
+                    Console.WriteLine("Does not compute");
+                    break;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
